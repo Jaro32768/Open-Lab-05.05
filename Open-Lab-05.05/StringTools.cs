f99b130180ceea3bd31cf7707b240cf79f2c844c
Jaro32768 <71781871+Jaro32768@users.noreply.github.com>
2020-10-19 18:36:12 +0200

    vypracovanie

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -6,7 +6,27 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            int k = 0;
+            string low = original.ToLower();
+            string ret = "";
+            for (int i = 0; i < low.Length; i++)
+            {
+                if (low[i] == ' ')
+                {
+                    ret = ret + low[i];
+                }
+                else if (k % 2 == 0)
+                {
+                    ret = ret + low[i].ToString().ToUpper();
+                    k = k + 1;
+                }
+                else if (k % 2 == 1)
+                {
+                    ret = ret + low[i].ToString().ToLower();
+                    k = k + 1;
+                }
+            }
+            return ret;
         }
     }
 }
